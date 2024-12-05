e12948353ffe7ae7e4867b2a3c3409bedc6b93a9
Warrior <kpanfas6@gmail.com>
2024-12-05 18:44:01 +0530

    python Programs

---
+++ python/tuples.py
@@ -0,0 +1,10 @@
+
+# tuples creation
+
+My_Tuples = (4,2,3,2) + (5,2,5,2)
+
+print(f"The Tuples {My_Tuples}")
+
+# List Comprehension
+My_List = [x for x in range(1,5)]
+print(f"The List {My_List}")
